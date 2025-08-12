43ae038a45ff8aea72a70dc850958da5794d0d04
centa-create <eloronegro2023@gmail.com>
2025-08-12 16:30:48 -0500

    cuatro

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'a2e4c8b7f1d9e3a6c5b8d7f2e1c4a9b6e7d3c2b1f8a6e5d4c3b2a1f9e8d7c6b5'
     # Configuración de caché
     CACHE_TYPE = os.getenv('CACHE_TYPE', 'SimpleCache')
     CACHE_DEFAULT_TIMEOUT = int(os.getenv('CACHE_DEFAULT_TIMEOUT', 300))
