5f531d06ff9f5c01998dd2fb23b11370f1540ff8
centa-create <eloronegro2023@gmail.com>
2025-08-22 10:55:25 -0500

    Update admin.py

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,7 +1,3 @@
-
-# --- Importaciones organizadas ---
-
-
 from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
 from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
